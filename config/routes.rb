fecefd39529d999feeac2a1d5165ee4425c8cd9d
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-06-05 03:32:38 +0000

    [Fin]名前ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
+  resources :books
+  
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
